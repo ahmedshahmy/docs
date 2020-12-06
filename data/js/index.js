7c5e3e4e1cb5ae13445cbdb7d65889645498cd27
ahmedshahmy <maa.shahmy@gmail.com>
2020-12-06 09:07:47 +0530

    test

--- data/js/index.js
+++ data/js/index.js
@@ -1,3 +1,14 @@
 console.log('Hello')
 let name='me';
 console.log(name);
+let person = {
+    name:'me',
+    age:33
+};
+console.log(person);
+
+function test(){
+    console.log("test")
+}
+
+test();
