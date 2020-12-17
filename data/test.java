55c756fd5c8973a84576f2c2a8b2f141ccf54f1c
ahmedshahmy <maa.shahmy@gmail.com>
2020-12-17 08:13:39 +0530

    ddd

---
+++ data/test.java
@@ -0,0 +1,18 @@
+class test{
+
+    public static void main(String a[]) {
+
+        System.out.println("hi");
+        
+    }
+}
+
+
+class hello{
+
+
+    public void hellom(){
+        System.out.println("x");
+
+    }
+}
